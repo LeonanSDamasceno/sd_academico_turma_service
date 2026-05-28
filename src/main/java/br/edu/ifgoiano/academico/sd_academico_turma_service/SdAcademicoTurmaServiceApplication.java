a307e0d2cc15040457d4c3269a0a779a831b1632
Artur Duarte Monteiro <monteiroarturduarte@gmail.com>
2026-05-28 13:05:35 -0300

    Initial commit

---
+++ src/main/java/br/edu/ifgoiano/academico/sd_academico_turma_service/SdAcademicoTurmaServiceApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifgoiano.academico.sd_academico_turma_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdAcademicoTurmaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdAcademicoTurmaServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifgoiano/academico/sd_academico_turma_service/SdAcademicoTurmaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifgoiano.academico.sd_academico_turma_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdAcademicoTurmaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
